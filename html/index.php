de047330c66d1f9b486cf12e640bce02b32a840b
Evgenii17Web <evgenii17pershin@gmail.com>
2021-07-06 15:11:45 +0300

    Задание #5.2 Twig

--- html/index.php
+++ html/index.php
@@ -3,8 +3,8 @@
 require '../src/config.php';
 require '../vendor/autoload.php';
 
-//$route = new \Base\Route();
-//$route->add('/', \App\Controller\Login::class);
-//
-//$app = new \Base\Application($route);
-//$app->run();
+$route = new \Base\Route();
+$route->add('/', \App\Controller\Login::class);
+
+$app = new \Base\Application($route);
+$app->run();
